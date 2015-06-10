7148b127f18185b021020a6109cd396c0d3e4e6c
Vladislav Gorbik <v.gorbik@doc.ua>
2015-06-10 09:31:45 +0300

    templates: parse file with parent template to one file

--- index.js
+++ index.js
@@ -7,26 +7,98 @@ module.exports = function() {
   var doReplace = function(file, enc, callback) {
     var jadeVars = '';
     var dataDir = __dirname.split('node_modules')[0] + 'src/templates/data';
+    var parentTplName = '';
     
     if (file.isNull()){
       return callback(null, file);
     }
+
+    function mixin(blockName, blockSpaces){
+      return  '  mixin dinamicMixin(data1, data2, data3)\n' +
+              blockSpaces + '    - data = {}\n' +
+              blockSpaces + '    - redefines = []\n' +
+              blockSpaces + '    - if (typeof data1 === "object") redefines.push(data1)\n' +
+              // blockSpaces + '    - da = JSON.stringify(data1)\n' +
+              // blockSpaces + '    p #{da}\n' +
+              blockSpaces + '    - for (var i in redefines)\n' +
+              blockSpaces + '      - for (var k in redefines[i])\n' +
+              blockSpaces + '        - if (redefines[i].hasOwnProperty(k))\n' +
+              blockSpaces + '          - data[k] = redefines[i][k]\n' +
+              blockSpaces + '    - data._bemto_chain = bemto_chain.slice()\n' +
+              blockSpaces + '    - blockName = bemto_chain[bemto_chain.length-1]\n' +
+              blockSpaces + '    include ../../templates/blocks/' + blockName + '/' + blockName + '.jade\n' +
+              blockSpaces + '  +dinamicMixin'
+    }
+
+
+    function bemReplace(fileContents){
+      var chunks = fileContents.split(': +i');
+      var result = '';
+      for (var i = 0; i < chunks.length; i++){
+        if(i < chunks.length - 1){
+            var block = chunks[i].split('\n')[chunks[i].split('\n').length - 1]
+            var blockSpaces = block.split('+b')[0]
+            var blockName = block.split('.')[1]
+            result += chunks[i] + '\n' + blockSpaces + mixin(blockName, blockSpaces);
+        }else{
+            result += chunks[i];
+        }
+      }
+
+      // console.log('result=', result)
+      result = jadeVars + result;
+      file.contents = new Buffer(result);
+
+      return callback(null, file);
+
+    }
+
+    function parseParentTpl(){
+        if(parentTplName){
+          var filePath = __dirname.split('node_modules')[0] + 'src/templates/' + parentTplName + '.jade';
+          fs.readFile(filePath, 'utf-8', function (err, parentTplData) {
+            var fileContents = String(file.contents);
+            var blockSpaces = parentTplData.split('block content')[0].split('\n')[parentTplData.split('block content')[0].split('\n').length - 1];
+            
+            var chunks = fileContents.split('\n');
+            // console.log('chunks=', chunks)
+            
+            fileContents = chunks[0]  + '\n';
+            for (var i = 1; i < chunks.length; i++){
+              fileContents += blockSpaces + chunks[i] + '\n'
+            }
+            // console.log('fileContents=', fileContents)
+
+            // console.log('parentTplData=', parentTplData.replace('block content', fileContents))
+            fileContents = parentTplData.replace('block content', fileContents)
+            
+            bemReplace(fileContents);
+            
+            // var fName = tpl.split('/')[tpl.split('/').length - 1];
+            // var fileOutPath = __dirname.split('node_modules')[0] + 'dev/layouts/' + fName + '.jade';
+            // fs.writeFile(fileOutPath, result, function (err) {
+            //   return callback(null, file);
+            // });
+
+          });
+        }
+      }
 
     function rFile(filesList, num){ //read file data
       if(num < filesList.length - 1){
         fs.readFile(dataDir + '/' + filesList[num], 'utf8', function (err, data) {
           // if (err) throw err;
           data = data.replace(/\r?\n|\r/g, '');
-          jadeVars += '  - ' + filesList[num].split('.')[0] + ' = ' + data + '\n';
+          jadeVars += '- ' + filesList[num].split('.')[0] + ' = ' + data + '\n';
           rFile(filesList, num  + 1);
         });
       }else{
-        var _chunks = String(file.contents).split(/extends/g);
-        _chunks = _chunks[1].split(/\n/g);
-        file.contents = new Buffer('extends' + _chunks[0] + '\n\n' + jadeVars + '\n' + _chunks.slice(1).join('\n'), 'utf-8');  
+        // var _chunks = String(file.contents).split(/\n\n/g);
+        // _chunks = _chunks[1].split(/\n/g);
         
-        // file.contents = new Buffer(file.contents);
-        return callback(null, file);
+        // file.contents = new Buffer(jadeVars, 'utf-8');
+        parseParentTpl()
+        // return callback(null, file);
       }
     }
 
@@ -41,7 +113,8 @@ module.exports = function() {
         for (var i = 1; i < fileVarsArr.length; i++){
           fileVars[fileVarsArr[i].split(':')[0].replace(/\s{0,}/g, '')] = fileVarsArr[i].split(':')[1].replace(/\s{0,}/g, '')
         };
-        jadeVars = 'block generated_params\n  - file = ' + JSON.stringify(fileVars) + '\n';
+        jadeVars = '- file = ' + JSON.stringify(fileVars) + '\n';
+        parentTplName = fileVarsArr[1].split(':')[1].replace(/\s{0,}/g, '');
       }
     }
 
